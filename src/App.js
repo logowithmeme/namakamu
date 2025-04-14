2a75773e59eb7e0ea24775cd591101b0828609b5
logowithmeme <logowithmeme@gmail.com>
2025-04-15 03:35:58 +0530

    ✅ Fixed routing: all pages wired up correctly in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-// src/App.js
+// App.js (Final routing with all pages)
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RoomCreated from './pages/RoomCreated';
-import JoinRoomPage from './pages/JoinRoomPage';
 import ChatPage from './pages/ChatPage';
+import JoinRoomPage from './pages/JoinRoomPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/room-created/:roomId" element={<RoomCreated />} />
-        <Route path="/join-room/:roomId" element={<JoinRoomPage />} />
+        <Route path="/room-created" element={<RoomCreated />} />
         <Route path="/chat/:roomId" element={<ChatPage />} />
+        <Route path="/join-room/:roomId" element={<JoinRoomPage />} />
       </Routes>
     </Router>
   );
